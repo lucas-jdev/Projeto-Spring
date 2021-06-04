7b6ff5a4e2b68b145bd0f6f4f8df5849a144e854
Lucas-dev-back <76585138+Lucas-dev-back@users.noreply.github.com>
2021-06-04 18:10:18 -0300

    Update AppApplicationTests.java

--- src/test/java/com/java/aplication1/app/AppApplicationTests.java
+++ src/test/java/com/java/aplication1/app/AppApplicationTests.java
@@ -19,34 +19,6 @@ class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
-		// Metodo convencional para valores estaticos
-		double n1 = 5;
-		double n2 = 6;
-		double n3 = 4;
-		double n4 = 7;
-		double mediaEstatica = (n1+n2+n3+n4)/4;
-		System.out.println(mediaEstatica);
-
-		// Metodo convecional para valores de entrada
-		Scanner in = new Scanner(System.in);
-		double mediaConvencional = 0;
-		double contador = 0;
-		for(int i = 0; i < 5; i++){
-			double n = in.nextDouble();
-			mediaConvencional += n;
-			contador++;
-		}
-		System.out.println(mediaConvencional/contador);
-
-		// Metodo que serve para ambos os casos e de forma bela
-		List<Double> real = new ArrayList<>();
-		real.add(5d);
-		real.add(6d);
-		real.add(4d);
-		real.add(7d);
-		double media = real.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
-		System.out.println(media);
 
 	}
 
